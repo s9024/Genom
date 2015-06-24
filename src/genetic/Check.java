f99d208d4cee54a60e2921d2ca05b4d15883ecb6
9024 <>
2015-06-24 20:46:21 +0200

    1.1

---
+++ src/genetic/Check.java
@@ -0,0 +1,12 @@
+
+package genetic;
+
+public class Check {
+
+int a = 2;
+int b = 3;
+
+    
+        
+    
+}
---
+++ src/genetic/Genetic.java
@@ -0,0 +1,15 @@
+
+package genetic;
+
+public class Genetic {
+   public static void main(String[] args) {
+       Check ch = new Check();
+       ch.a = 6;
+       System.out.println("a");
+       System.out.println(ch.a);
+       int x = ch.a * ch.b;
+       System.out.println(x);
+
+    }
+    
+}
